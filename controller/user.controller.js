c335f42255fc847526b30fa0eea52f7b4cc38a89
Abexstar <abexstar@gmail.com>
2024-11-27 12:08:21 +0100

    update

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,34 @@
-const registerUser = () => {
+const User = require("../model/user.model.js");
+const { passwordHasher } = require("../utils/helper.js");
+const registerUser = async (req, res, next) => {
+    const { name, email, phone, password} = req.body;
+    try {
+
+        if(!name || !email || !password){
+            return res.status(400).json({ message: "All fields are required." });
+        }
+
+        const userExists = await User.findOne({email});
+        if(userExists){
+            return res.status(400).json({ message: "User already exists." });
+        }
+
+        if(phone){
+            const phoneExists = await User.findOne({mobile: phone});
+            if (phoneExists){
+                return res.status(400).json({ message: "Phone number already exists." });
+            }
+        }
+
+        const hashedPassword = await passwordHasher(password);
+        const user = new User({ name, email, mobile: phone, password: hashedPassword });
+        await user.save();
+        res.status(201).json({ message: "User registered successfully." });
+
+        
+    } catch (error) {
+        next(error);
+    }
 
 }
 
--- index.js
+++ index.js
@@ -7,10 +7,17 @@ const postRoute = require("./routes/post.route");
 
 const app = express();
 const PORT = process.env.PORT || 3500;
+app.use(express.json());
 mongoDbConnect()
 
-app.use("api/v1/user", userRoute);
-app.use("api/v1/post", postRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
+
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        error: err.message
+    });
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
